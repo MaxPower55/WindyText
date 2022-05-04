c7e640622d164d3e2046150844b8415b6e8ca4ec
MaxPower <andrewrubanov@gmail.com>
2022-05-04 12:40:54 +0600

    complete

---
+++ script.js
@@ -0,0 +1,6 @@
+const text = document.querySelector('.text');
+text.innerHTML = text.textContent.replace(/\S/g, '<span>$&</span>');
+text.addEventListener('mouseover', function(event) {
+    if (event.target.tagName != 'SPAN') return;
+    event.target.classList.add('active');
+});
